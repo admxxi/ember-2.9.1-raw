77ff807f81e3b4edc893edaa752ddc098e36c42f
Andre Monteiro <admxxi@gmail.com>
2016-11-10 23:31:40 +0000

    JS to Coffee

--- config/environment.js
+++ config/environment.js
@@ -3,6 +3,7 @@
 module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'ember-app',
+    podModulePrefix: 'ember-app/pods',
     environment: environment,
     baseURL: '/',
     locationType: 'auto',
--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,6 +5,15 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
     // Add options here
+    sassOptions: {
+      extension: 'scss'
+    },
+    minifyJS: {
+      enabled: false
+    },
+    minifyCSS: {
+      enabled: false
+    }
   });
 
   // Use `app.import` to add additional libraries to the generated
